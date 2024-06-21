5304991cbdf16df93ef54e9317cb19f1ffb38e7d
MituBanerjee <160964186+MituBanerjee@users.noreply.github.com>
2024-06-20 23:17:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,10 @@ st.line_chart(sales_by_month, y="Sales")
 st.write("## Your additions")
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
 st.dataframe(df.groupby("Category").sum())
+###1category = st.selectbox("Select a Category", df['Category'].unique())
 
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
-
+###2 sub_categories = st.multiselect("Select Sub_Categories", df[df['Category'] == category]['Sub_Category'].unique())
 
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 
