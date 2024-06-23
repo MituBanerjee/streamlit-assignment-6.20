fbe8aee2d7ac515dff3db12b7e7d95ff7e447c38
MituBanerjee <160964186+MituBanerjee@users.noreply.github.com>
2024-06-23 18:21:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ profit_margin_delta = overall_profit_margin - overall_avg_profit_margin
 st.write(f"Delta from Overall Average Profit Margin: {profit_margin_delta:.2f}%")
 
 else:
-st.write("Please select at least one sub-category to see the line chart and metrics.")
+  st.write("Please select at least one sub-category to see the line chart and metrics.")
 
 st.write("## Your additions")
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
